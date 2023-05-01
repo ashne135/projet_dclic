2778fdde62a63b87cec615c2ecf40f3f20aa525a
unknown <ashneouedraogo@gmail.com>
2023-05-02 00:49:26 +0200

    first commit

--- js/app.js
+++ js/app.js
@@ -33,4 +33,49 @@ closeBtn.addEventListener("click", () => {
   menuBtn.classList.remove("hidden");
   sidebar.classList.remove("open");
 });
+// On définit la fonction qui permet de changer la couleur du background au survol des liens
+function changeLinkColor() {
+    var links = document.getElementsByTagName("a");
+    for (var i = 0; i < links.length; i++) {
+      links[i].addEventListener("mouseover", function() {
+        this.style.color = "#333";
+      });
+      links[i].addEventListener("mouseout", function() {
+        this.style.color = "#666";
+      });
+    }
+  }
+  
+  // On appelle la fonction au chargement de la page
+  window.onload = function() {
+    changeLinkColor();
+  };
+  const form = document.getElementById("contact-form");
+
+form.addEventListener("submit", e => {
+  e.preventDefault();
+
+  const name = form.elements["name"].value;
+  const email = form.elements["email"].value;
+  const message = form.elements["message"].value;
+
+  const data = new FormData();
+  data.append("name", name);
+  data.append("email", email);
+  data.append("message", message);
+
+  fetch("submit.php", {
+    method: "POST",
+    body: data
+  })
+  .then(response => {
+    if (response.ok) {
+      alert("Votre message a été envoyé !");
+      form.reset();
+    } else {
+      alert("Une erreur est survenue, veuillez réessayer plus tard.");
+    }
+  });
+});
+
 
